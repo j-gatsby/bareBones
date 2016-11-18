27e75806f02d001bd11160c1a04a28969e1aefad
j-gatsby <timjoeckel@gmail.com>
2016-11-18 07:23:06 -0600

    Add comments to build.sh

--- build.sh
+++ build.sh
@@ -1,19 +1,21 @@
 set -e
 
-echo Compiling bootloader...
+echo Assembling bootloader...
 i686-elf-as boot.s -o boot.o
 
 echo Compiling kernel...
 i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 
-echo Linking...
+echo Linking object files...
 i686-elf-gcc -T linker.ld -o myOS.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
+echo NOTE: We use the compiler as the linker, as it
+echo allows for greater control over the link process.
 
 echo Verifying multiboot header...
 if grub-file --is-x86-multiboot myOS.bin; then
-	echo multiboot confirmed
+	echo Multiboot confirmed
 else
-	echo file is not multiboot
+	echo File is not multiboot
 fi
 
 echo Success.
